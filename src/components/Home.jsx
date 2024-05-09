aea9021b878afd6fc5d3c18c4872801bf7d55623
sebramirezm <s.ramirezm@icloud.com>
2024-05-08 23:45:27 -0400

    Migrate from CRA to Vite.js to handle bundling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import profileImg from '../assets/images/profile.jpeg'
+import resume from '../assets/files/SebastianRamirez_EN-Resume.pdf'
 import '../styles/Home.css';
 
 function Home() {
   return (
     <div className="FirstElement" id="home">
       <div className="profile-photo" data-aos="fade-right">
-        <img src="files/profile.jpeg" alt="profile picture" oncontextmenu="return false;"/>
+        <img src={profileImg} alt="profile picture" oncontextmenu="return false;"/>
       </div>
       <div className="profile-text" data-aos="fade-left">
         <h5><FormattedMessage id="home.greeting" /></h5>
@@ -17,7 +19,7 @@ function Home() {
           <FormattedMessage id="home.description" />
         </p>
         <div className="btn-group">
-          <a href="files/SebastianRamirez_EN-Resume.pdf" 
+          <a href={resume}
             className="btn active" 
             target="_blank"
             rel="noopener noreferrer">
@@ -29,10 +31,10 @@ function Home() {
         </div>
         <div className="social">
           <a href="https://www.linkedin.com/in/sebramirezm/" target="_blank">
-            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-linkedin" alt="LinkedIn Profile"></i>
           </a>
           <a href="https://github.com/sebramirezm" target="_blank">
-            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-github" alt="Github Repository"></i>
           </a>
         </div>
       </div>
